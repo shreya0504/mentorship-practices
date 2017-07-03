c3399543f431fc8a193c07250f4262b66fd30592
Shreya Bansal <shbansal@localhost.localdomain>
2017-07-03 15:27:56 +0530

    Intial Check in

--- src/session_001/practice1.js
+++ src/session_001/practice1.js
@@ -1,3 +1,17 @@
 (function() {
   // Todo
+var date = new Date();
+var currentMonth = date.getMonth();
+var currentDate = date.getDate();
+if(currentMonth<10)
+{
+  currentMonth= '0'+currentMonth;
+}
+if(currentDate<10)
+{
+  currentDate= '0'+currentDate;
+}
+document.write(date.getFullYear()+'-'+currentMonth+'-'+currentDate+'  '+date.getHours() + ":"
++ date.getMinutes() + ":" + date.getSeconds());
+
 })();
